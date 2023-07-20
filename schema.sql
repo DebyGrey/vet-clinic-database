9ef58d64302712bed4f3eaaa4305e0b906630ae0
DebyGrey <demifashoro@gmail.com>
2023-07-20 20:45:08 +0100

    Create database and animals table

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal(10, 2)
+
+);

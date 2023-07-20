c0b2fcfd0af0d35a0dc727902e2cac6d2458fd35
DebyGrey <demifashoro@gmail.com>
2023-07-21 00:34:49 +0100

    updated data.sql

--- data.sql
+++ data.sql
@@ -18,7 +18,7 @@ INSERT INTO owners(full_name, age)VALUES('Sam Smith', 34);
 
 INSERT INTO owners(full_name, age)VALUES('Jennifer Orwell', 19);
 
-INSERT INTO owners(full_name, age)VALUES('Bob', 19);
+INSERT INTO owners(full_name, age)VALUES('Bob', 45);
 
 INSERT INTO owners(full_name, age)VALUES('Melody Pond', 77);
 
